00bdee5b7b22e92c0fdb5a3fc987705d1125626b
Alexander <47594973+GloryToUkraine1@users.noreply.github.com>
2020-06-06 21:49:55 +0300

    Update task1.java

--- task1.java
+++ task1.java
@@ -1,7 +1,7 @@
 import java.util.*;
 
 
-public class Task1 {
+public class task1 {
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
 		System.out.println("1. Encrypt\n2. Decrypt");
